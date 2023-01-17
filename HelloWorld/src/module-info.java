e95a365c9f1defdd4bd4edbab72965a6e3e46e95
Angelo Hernandez <122906206+AngeloHernandezSAIT@users.noreply.github.com>
2023-01-17 09:50:26 -0700

    Add files via upload

---
+++ HelloWorld/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Cyber
+ *
+ */
+module HelloWorld {
+}
---
+++ HelloWorld/src/whatever/Hello.java
@@ -0,0 +1,10 @@
+package whatever;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+
+	}
+
+}
